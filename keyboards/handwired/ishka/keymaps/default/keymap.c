f46bfd28cf800fb42cc741ea956675a8d72cbdb5
Jordi Cunillera <jordicunillera1@gmail.com>
2024-08-26 00:29:22 +0200

    Keymap v0.1

--- keyboards/handwired/ishka/keymaps/default/keymap.c
+++ keyboards/handwired/ishka/keymaps/default/keymap.c
@@ -26,11 +26,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		),
 	[1] = LAYOUT(
 		KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,                                KC_TRNS,     KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS, 
-		KC_TRNS,      KC_HOME,      KC_UP,        KC_END,       KC_TRNS,                                KC_PMNS,     KC_P7,        KC_P8,        KC_P9,        KC_PSLS, 
-		KC_MPLY,      KC_LEFT,      KC_DOWN,      KC_RGHT,      KC_TRNS,                                KC_PPLS,     KC_P4,        KC_P5,        KC_P6,        KC_PAST, 
-		QK_BOOT,      KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_RALT,                                KC_P0,       KC_P1,        KC_P2,        KC_P3,        KC_RPRN, 
+		KC_TRNS,      KC_HOME,      KC_UP,        KC_END,       KC_TRNS,                                KC_PMNS,     KC_7,         KC_8,         KC_9,         KC_PSLS, 
+		KC_MPLY,      KC_LEFT,      KC_DOWN,      KC_RGHT,      KC_TRNS,                                KC_PPLS,     KC_4,         KC_5,         KC_6,         KC_PAST, 
+		QK_BOOT,      KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_RALT,                                KC_0,        KC_1,         KC_2,         KC_3,         KC_RPRN, 
 		                            KC_LSFT,      KC_TRNS,      KC_SPC,                                 KC_ENT,      KC_TRNS,      KC_LCTL
-		), 
+		),
 	[2] = LAYOUT(
 		KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,                                KC_TRNS,     KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS, 
 		KC_WH_U,      KC_BTN1,      KC_MS_U,      KC_BTN2,      KC_TRNS,                                KC_TRNS,     KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS, 
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC,       KC_1,         KC_2,         KC_3,         KC_4,                                   KC_5,        KC_6,         KC_7,         KC_8,         KC_9, 
 		KC_TAB,       KC_Q,         KC_W,         KC_E,         KC_R,                                   KC_T,        KC_Y,         KC_U,         KC_I,         KC_O, 
 		KC_LSFT,      KC_A,         KC_S,         KC_D,         KC_F,                                   KC_G,        KC_H,         KC_J,         KC_K,         KC_L, 
-		KC_LCTL,      KC_Z,         KC_X,         KC_C,         KC_V,                                   KC_B,        KC_N,         KC_M,         KC_MPLY,      KC_P, 
+		KC_LCTL,      KC_Z,         KC_X,         KC_C,         KC_V,                                   KC_B,        KC_N,         KC_M,         KC_P,         KC_MPLY, 
 		                            MO(4),        KC_LALT,      KC_SPC,                                 KC_ENT,      TG(3),        KC_RALT
 		),
 	[4] = LAYOUT(
